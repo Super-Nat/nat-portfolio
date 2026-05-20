b0dd0388a46763ed056b930ea25be326196f11a4
Natchapon-Blockfint <nuttsumc@gmail.com>
2026-05-20 23:26:12 +0700

    Add next-nprogress-bar for progress indication: update package.json and pnpm-lock.yaml, and integrate ProgressBar in AdminProviders component.

--- src/components/providers/AdminProviders.tsx
+++ src/components/providers/AdminProviders.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import { Toaster } from "../ui/sonner";
 
 const queryClient = new QueryClient();
@@ -12,6 +13,12 @@ const AdminProviders = ({
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ProgressBar
+        height="2px"
+        color="#000000"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
       <Toaster position="bottom-right" />
       {process.env.NODE_ENV === "development" && (
         <ReactQueryDevtools initialIsOpen={false} />
